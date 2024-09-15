195522dc1684997fa68fa0c9b18cf8d9be578f35
andr3w-coderman <admyrden@gmail.com>
2024-09-15 19:52:48 -0400

    Change Frameworks
    -Change from Bootstrap to Tailwind
    -Add DaisyUI module

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './**/*.{html,js}',   // Scan your project for HTML and JS files
+      '!./node_modules/**/*' // Exclude node_modules directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('daisyui'),
+  ],
+}
+
