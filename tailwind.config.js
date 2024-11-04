3e31e5870ca48d6b11035f877dc1d1f11b388c71
Andrew <admyrden@gmail.com>
2024-11-03 20:13:58 -0500

    Update color scheme
    -Attempting to make an animated gradient background, and override some stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,23 @@ module.exports = {
       './**/*.{html,js}',   // Scan your project for HTML and JS files
       '!./node_modules/**/*' // Exclude node_modules directory
   ],
-  theme: {
-    extend: {},
-  },
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-animation': 'linear-gradient(270deg, #bababa, #dbdbdb)',
+            },
+            animation: {
+                'gradient-move': 'gradientAnimation 3s ease infinite',
+            },
+            keyframes: {
+                gradientAnimation: {
+                    '0%': {'background-position': '0% 50%'},
+                    '50%': {'background-position': '100% 50%'},
+                    '100%': {'background-position': '0% 50%'},
+                },
+            },
+        },
+    },
   plugins: [
       require('daisyui'),('@tailwindcss/aspect-ratio')
   ],
